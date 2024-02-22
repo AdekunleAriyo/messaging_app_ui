a2776b86ae42ce4009c17abbb1d5276a19ee2892
AdekunleAriyo <dekunlearry@gmail.com>
2024-02-22 23:48:22 +0100

    slight changes

--- lib/widgets/favorite_contacts.dart
+++ lib/widgets/favorite_contacts.dart
@@ -7,67 +7,70 @@ class FavoriteContacts extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: EdgeInsets.all(10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Text(
-                'Favorite Contacts',
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                    color: Colors.blueGrey,
-                    letterSpacing: 1.0),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: Padding(
-                  padding: const EdgeInsets.only(right: 20.0),
-                  child: Icon(
-                    Icons.more_horiz,
-                  ),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'Favorite Contacts',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                      color: Colors.blueGrey,
+                      letterSpacing: 1.0),
                 ),
-                iconSize: 30,
-                color: Colors.blueGrey,
-              )
-            ],
-          ),
-        ),
-        Container(
-          height: 120,
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            padding: EdgeInsets.only(left: 10),
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    CircleAvatar(
-                      radius: 35,
-                      backgroundImage: AssetImage(
-                        favorites[index].imageUrl,
-                      ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Padding(
+                    padding: const EdgeInsets.only(right: 20.0),
+                    child: Icon(
+                      Icons.more_horiz,
                     ),
-                    SizedBox(height: 6.0),
-                    Text(
-                      favorites[index].name,
-                      style: TextStyle(
-                          color: Colors.blueGrey,
-                          fontWeight: FontWeight.w600,
-                          fontSize: 16),
-                    )
-                  ],
-                ),
-              );
-            },
-            itemCount: favorites.length,
+                  ),
+                  iconSize: 30,
+                  color: Colors.blueGrey,
+                )
+              ],
+            ),
           ),
-        )
-      ],
+          Container(
+            height: 120,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              padding: EdgeInsets.only(left: 10),
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 35,
+                        backgroundImage: AssetImage(
+                          favorites[index].imageUrl,
+                        ),
+                      ),
+                      SizedBox(height: 6.0),
+                      Text(
+                        favorites[index].name,
+                        style: TextStyle(
+                            color: Colors.blueGrey,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 16),
+                      )
+                    ],
+                  ),
+                );
+              },
+              itemCount: favorites.length,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
