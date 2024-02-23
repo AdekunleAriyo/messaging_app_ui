a7d8fb4350ba7291e8edf2de9b2d4c525b13a7c0
AdekunleAriyo <dekunlearry@gmail.com>
2024-02-23 19:47:57 +0100

    last commit

--- lib/widgets/recent_chats.dart
+++ lib/widgets/recent_chats.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:messaging_app_ui/screens/chat_screen.dart';
 
 import '../models/message_model.dart';
 
@@ -23,58 +24,93 @@ class RecentChats extends StatelessWidget {
           child: ListView.builder(
             itemBuilder: (context, index) {
               final Message chat = chats[index];
-              return Container(
-                margin: EdgeInsets.only(top: 5, bottom: 5, right: 20, left: 10),
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                decoration: BoxDecoration(
-                    color: chat.unread ? Color(0xFFFFEFEE) : Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(20),
-                      topLeft: Radius.circular(20),
-                    )),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        CircleAvatar(
-                          radius: 35,
-                          backgroundImage: AssetImage(chat.sender.imageUrl),
-                        ),
-                        SizedBox(width: 10),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              chats[index].sender.name,
-                              style: TextStyle(
-                                  color: Colors.grey,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 15),
-                            ),
-                            SizedBox(height: 10),
-                            Container(
-                              width: MediaQuery.of(context).size.width * 0.45,
-                              child: Text(
-                                chat.text,
+              return GestureDetector(
+                onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (_) => ChatScreen(
+                              user: chat.sender,
+                            ))),
+                child: Container(
+                  margin:
+                      EdgeInsets.only(top: 5, bottom: 5, right: 20, left: 10),
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  decoration: BoxDecoration(
+                      color: chat.unread ? Color(0xFFFFEFEE) : Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(20),
+                        topLeft: Radius.circular(20),
+                        bottomLeft: Radius.circular(20),
+                        bottomRight: Radius.circular(20),
+                      )),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          CircleAvatar(
+                            radius: 35,
+                            backgroundImage: AssetImage(chat.sender.imageUrl),
+                          ),
+                          SizedBox(width: 10),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                chats[index].sender.name,
                                 style: TextStyle(
-                                    color: Colors.blueGrey,
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.w600),
-                                overflow: TextOverflow.ellipsis,
+                                    color: Colors.grey,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 15),
                               ),
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Text(chat.time),
-                        Text('NEW'),
-                      ],
-                    )
-                  ],
+                              SizedBox(height: 10),
+                              Container(
+                                width: MediaQuery.of(context).size.width * 0.45,
+                                child: Text(
+                                  chat.text,
+                                  style: TextStyle(
+                                      color: Colors.blueGrey,
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w600),
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            chat.time,
+                            style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(height: 5),
+                          chat.unread
+                              ? Container(
+                                  width: 40,
+                                  height: 20,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(30),
+                                    color: Theme.of(context).primaryColor,
+                                  ),
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                    'NEW',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                )
+                              : Text(''),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               );
             },
